5836c8f6b2e8e9039bfd2278f26d5ac877cb9ca3
Rodriques <rodriques.perry@gmail.com>
2022-04-20 21:43:51 -0500

    React component rewrite

--- src/App.js
+++ src/App.js
@@ -1,44 +1,42 @@
-
 const App = () => {
+
+  const categories = [
+    {
+      id: 1,
+      title: 'Hats',
+    },
+    {
+      id: 2,
+      title: 'Jackets',
+    },
+    {
+      id: 1,
+      title: 'Sneakers',
+    },
+    {
+      id: 1,
+      title: 'Womens',
+    },
+    {
+      id: 1,
+      title: 'Mens',
+    },
+  ];
+
   return (
-  <div className="categories-container">   
-    <div className="category-container">
-        {/**<img />> */}
-        <div className="category-body-container">
-          <h2>Hats</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className="category-container">
-        {/**<img />> */}
-        <div className="category-body-container">
-          <h2>Jackets</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className="category-container">
-        {/**<img />> */}
-        <div className="category-body-container">
-          <h2>Sneakers</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className="category-container">
-        {/**<img />> */}
-        <div className="category-body-container">
-          <h2>Womens</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className="category-container">
-        {/**<img />> */}
-        <div className="category-body-container">
-          <h2>Mens</h2>
-          <p>Shop Now</p>
-      </div>
+    <div className="categories-container">
+      {categories.map(({title, id}) => (
+          <div className="category-container" key={id}>
+           {/**<img />> */}
+           <div className="category-body-container">
+              <h2>{title}</h2>
+              <p>Shop Now</p>
+            </div>
+          </div>
+        )
+      )}
     </div>
-  </div>
   );
-}
+};
 
 export default App;
