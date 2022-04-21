6a51d221c5904deb60438a7fd43e4b8aa3cb5e1b
Rodriques <rodriques.perry@gmail.com>
2022-04-21 14:11:04 -0500

    update: BrowserRouter

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import Home from "./routes/home/home.component";
 import { Routes, Route } from "react-router-dom";
 
 const App = () => {
-  return (
-    {/**
+  {/**
       Step 1: import BrowserRouter and wrap App with it in index.js
       Step 2: import Routes and Route as shown above
       Step 3: Wrap element which is Route in Routes and add attributes
     */}
+  return (
     <Routes>
       <Route path="/home" element={<Home />} />
     </Routes>
