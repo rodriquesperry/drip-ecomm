8309743b4c14c7b48c913a016b87bb0774e97dd5
Rodriques <rodriques.perry@gmail.com>
2022-04-21 15:34:08 -0500

    update: add assests and routes navigation

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.component";
+
 import { Routes, Route } from "react-router-dom";
 
+const Shop = () => {
+  return (
+    <div>
+      <h1>I am the shop page</h1>
+    </div>
+  );
+};
+
 const App = () => {
-  {/**
+  {
+    /**
       Step 1: import BrowserRouter and wrap App with it in index.js
       Step 2: import Routes and Route as shown above
       Step 3: Wrap element which is Route in Routes and add attributes
-    */}
+    */
+  }
   return (
     <Routes>
-      <Route path="/home" element={<Home />} />
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+      </Route>
     </Routes>
   );
 };
