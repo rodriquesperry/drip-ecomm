60db51c564ae1748bebdb6ebde432845547c2840
Rodriques <rodriques.perry@gmail.com>
2022-04-22 11:38:02 -0500

    update: createUserWithEmailAndPassword()

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -5,6 +5,7 @@ import {
   signInWithRedirect,
   signInWithPopup,
   GoogleAuthProvider,
+  createUserWithEmailAndPassword,
 } from "firebase/auth";
 import {
   getFirestore,
@@ -85,4 +86,9 @@ export const createUserDocumentFromAuth = async (userAuth) => {
   //   if user data exists
 
   return userDocRef;
+};
+
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) return;
+  return await createUserWithEmailAndPassword(auth, email, password);
 };
