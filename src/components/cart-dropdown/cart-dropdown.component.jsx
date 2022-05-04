6d3a244d7d152998fc367a993d2c294475111a63
Rodriques <rodriques.perry@gmail.com>
2022-05-04 17:55:48 -0500

    update: add remove item functionality to dropdown cart

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -4,6 +4,7 @@ import { CartItemContext } from "../../context/cart-item.context";
 
 import Button from "../button/button.component";
 import CartItem from "../cart-item/cart-item.component";
+import { ReactComponent as Remove } from "../../asstes/x.svg";
 
 import "./cart-dropdown.styles.scss";
 
@@ -13,13 +14,13 @@ const CartDropdown = () => {
 
   const handleGoToCheckout = () => {
     navigate("/checkout");
-  }
+  };
 
   return (
     <div className="cart-dropdown-container">
       <div className="cart-items">
         {cartItems.map((item) => (
-          <CartItem key={item.id} cartItem={item} />
+            <CartItem key={item.id} cartItem={item} />
         ))}
         <Button type="button" onClick={handleGoToCheckout}>
           Go To Checkout
--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -1,6 +1,13 @@
+import { useContext } from "react";
+import { CartItemContext } from "../../context/cart-item.context";
+
+import { ReactComponent as Remove } from "../../asstes/x.svg";
+
 import "./cart-item.styles.scss";
 
 const CartItem = ({ cartItem }) => {
+  const { removeItemFromCheckout } = useContext(CartItemContext);
+
   const { imageUrl, name, quantity, price } = cartItem;
   return (
     <div className="cart-item-container">
@@ -9,6 +16,12 @@ const CartItem = ({ cartItem }) => {
         <span className="name">{name}</span>
         <span>
           {quantity} x ${price}
+        </span>
+        <span>
+          <Remove
+            onClick={() => removeItemFromCheckout(cartItem)}
+            className="remove-icon"
+          />
         </span>
       </div>
     </div>
